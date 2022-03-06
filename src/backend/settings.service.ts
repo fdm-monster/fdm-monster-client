52ebf572b7aea3eb1a7fb7fcfb8b41edc417710d
David Zwart <davidzwa@gmail.com>
2022-03-06 13:44:21 +0100

    Add backend, fix typescript changes with type imports, fix models

---
+++ src/backend/settings.service.ts
@@ -0,0 +1,30 @@
+import { ServerApi } from "@/backend/server.api";
+import { BaseService } from "@/backend/base.service";
+import type  {
+  PrinterFileCleanSubSetting,
+  ServerSettings
+} from "@/models/server-settings/server-settings.model";
+import type { PrinterFileCleanSettings } from "@/models/server-settings/printer-file-clean-settings.model";
+import type { ClientSettings } from "@/models/client-settings/client-settings.model";
+
+export class SettingsService extends BaseService {
+  static async getServerSettings() {
+    const path = ServerApi.serverSettingsRoute;
+
+    return (await this.getApi(path)) as ServerSettings;
+  }
+
+  static async getClientSettings() {
+    const path = ServerApi.clientSettingsRoute;
+
+    return (await this.getApi(path)) as ClientSettings;
+  }
+
+  static async setFileHandlingClientSettings(subSettings: PrinterFileCleanSettings) {
+    const path = `${ServerApi.serverSettingsRoute}`;
+
+    return (await this.putApi(path, {
+      printerFileClean: subSettings
+    } as PrinterFileCleanSubSetting)) as ServerSettings;
+  }
+}
