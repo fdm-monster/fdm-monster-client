718354ae3b55c95b5f3538166c72fd970c629c47
David Zwart <davidzwa@gmail.com>
2022-03-06 15:10:10 +0100

    Add alpha vuetify - minor 13

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+import { createPinia } from 'pinia'
 import { vueErrorHandler } from './handlers/error.handler';
 
-const app = createApp(App);
+loadFonts()
+
+const app = createApp(App)
+  .use(router)
+  .use(createPinia())
+  .use(vuetify);
+
 app.config.errorHandler = vueErrorHandler;
-
-app.use(createPinia());
-app.use(router);
-
 app.mount('#app');
