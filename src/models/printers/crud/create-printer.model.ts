740154e09d2529c94462384de4d3ed04db11e225
David Zwart <davidzwa@gmail.com>
2023-08-13 09:28:47 +0200

    fix: clear commented out code in model

--- src/models/printers/crud/create-printer.model.ts
+++ src/models/printers/crud/create-printer.model.ts
@@ -1,45 +1,28 @@
-import { newRandomNamePair } from "../../../shared/noun-adjectives.data";
+import { newRandomNamePair } from "@/shared/noun-adjectives.data";
 
 export type HttpProtocol = "http" | "https";
 
 export const getDefaultCreatePrinter = (): PreCreatePrinter => ({
   id: undefined,
   printerName: newRandomNamePair(),
-  // printerHostPrefix: "http",
-  // printerHostPort: 80,
-  // printerHostName: "",
   apiKey: "",
   printerURL: "",
   enabled: true,
 });
 
-/**
- * DEPRECATED over ZOD schema + infer
- */
 export interface PreCreatePrinter {
   id?: string; // Only in case of update
 
   enabled: boolean;
   printerName: string;
-
-  // printerHostPrefix: HttpProtocol;
-  // printerHostName: string;
-  // printerHostPort: number | undefined;
   printerURL: string | undefined;
-
   apiKey: string;
 }
 
-/**
- * DEPRECATED over ZOD schema + infer
- */
 export interface CreatePrinter {
   id?: string; // Only in case of update
-
   enabled: boolean;
   printerName: string;
-
   printerURL: string;
-
   apiKey: string;
 }
