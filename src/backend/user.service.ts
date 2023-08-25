a7e2c88e3522b75d1311c11a6cbadb5c41d7d1a0
David Zwart <davidzwa@gmail.com>
2023-08-25 17:46:39 +0200

    feat: add new UserService

--- src/backend/user.service.ts
+++ src/backend/user.service.ts
@@ -6,6 +6,11 @@ export class UserService extends BaseService {
   static async listUsers() {
     const path = ServerApi.userRoute;
 
-    return (await this.getApi(path)) as User[];
+    return await this.getApi<User[]>(path);
+  }
+
+  static async getProfile() {
+    const path = ServerApi.userProfileRoute;
+    return await this.getApi<User>(path);
   }
 }
