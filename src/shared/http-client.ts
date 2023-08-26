bf3801fa5e0d395c9c4672cff6063599c25a4b4d
David Zwart <davidzwa@gmail.com>
2023-08-26 14:17:20 +0200

    stash: more clean http client interceptor

--- src/shared/http-client.ts
+++ src/shared/http-client.ts
@@ -15,15 +15,105 @@ export async function getBaseUri() {
 }
 
 export async function getHttpClient(withAuth: boolean = true) {
-  axios.interceptors.request.use(async (config) => {
-    config.baseURL = await getBaseUri();
-    if (withAuth) {
-      const auth = useAuthStore();
-      if (auth.isLoggedIn) {
-        config.headers.Authorization = `Bearer ${auth.token}`;
-      }
-    }
+  const instance = axios.create({
+    baseURL: await getBaseUri(),
+  });
+
+  instance.interceptors.request.use((config) => {
+    console.log(
+      `${config.method?.toUpperCase().padEnd(6)} ${config.url?.padEnd(25)} Bearer: ${withAuth
+        ?.toString()
+        .padEnd(7)} BaseURL: ${config.baseURL}`
+    );
     return config;
   });
-  return axios;
+
+  if (withAuth) {
+    instance.interceptors.request.use(
+      async (config) => {
+        const auth = useAuthStore();
+        if (auth.hasAuthToken) {
+          config.headers.Authorization = `Bearer ${auth.token}`;
+        }
+
+        return config;
+      },
+      (error) => {
+        console.error("Error in axios request interceptor", error);
+        return Promise.reject(error);
+      }
+    );
+  }
+
+  instance.interceptors.response.use(
+    (response) => response,
+    async (error: AxiosError) => {
+      const { response, config } = error;
+      if (!response) {
+        console.error("No response was returned by axios", error);
+        return error;
+      }
+
+      if (![HttpStatusCode.Unauthorized, HttpStatusCode.Unauthorized].includes(response.status)) {
+        // Timeout issues etc?
+        console.error("Error in axios response interceptor which is not 401 or 403", error);
+        return error;
+      }
+
+      if (response.status === HttpStatusCode.Unauthorized) {
+        // TODO exempt hostnames from retries
+        // TODO circuit breaker (max attempts) to prevent infinite retries
+        // TODO retry with exponential backoff
+        // TODO retry with extended timeout
+        // TODO consider axios-retry package
+
+        // try {
+        //   await authStore.refreshTokens();
+        // } catch (e) {
+        //   console.error("Error when refreshing login.");
+        //   snackbar.openErrorMessage({
+        //     title: "Login error",
+        //     fullSubtitle: "Error when refreshing login.",
+        //   });
+        //   await router.push({ name: "Login" });
+        // }
+        if (!withAuth) {
+          console.warn(`${error.config?.url} No auth was required, but 401 was thrown`);
+          useSnackbar().openErrorMessage({
+            title: "Authentication bug",
+            fullSubtitle:
+              "Login was required, although the request was not done with authentication. This is a bug.",
+          });
+          // throw error;
+          return;
+        }
+
+        // Check if state has changed, if so then retry
+        const authStore = useAuthStore();
+        if (!authStore.loginRequired) {
+          await authStore.checkLoginRequired();
+          if (authStore.loginRequired) {
+            console.warn("Retrying request after new login state");
+            await sleep(100);
+            return axios(config as AxiosRequestConfig);
+          } else {
+            console.warn("Cannot retry request as login is still not required");
+            useSnackbar().openErrorMessage({
+              title: "Authentication bug",
+              fullSubtitle:
+                "Authentication was not required, but the server returned authentication error. This is a bug.",
+            });
+            return;
+          }
+        } else {
+          console.warn("Login was required and 401 was thrown");
+        }
+
+        // Login required, so check expiry
+      } else if (response.status === HttpStatusCode.Forbidden) {
+        await useRouter().push({ name: RouteNames.LackingPermission });
+      }
+    }
+  );
+  return instance;
 }
