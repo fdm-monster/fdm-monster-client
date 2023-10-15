ca441526b104083e677f71ce738d90257372e62f
David Zwart <davidzwa@gmail.com>
2023-10-15 21:14:34 +0200

    chore: string literal adjustment

--- src/backend/app.service.ts
+++ src/backend/app.service.ts
@@ -10,19 +10,19 @@ export class AppService extends BaseService {
   }
 
   static async getClientReleases() {
-    return (await this.getApi(`api/server/client-releases`)) as IClientReleases;
+    return (await this.getApi("api/server/client-releases")) as IClientReleases;
   }
 
   static async getVersion() {
-    return (await this.getApi(`api/version`)) as VersionModel;
+    return (await this.getApi("api/version")) as VersionModel;
   }
 
   static async getFeatures() {
-    return (await this.getApi(`api/features`)) as FeaturesModel;
+    return (await this.getApi("api/features")) as FeaturesModel;
   }
 
   static async test() {
     const httpClient = await getHttpClient(false, false);
-    return (await httpClient.get(`api/test`)) as { message: string };
+    return (await httpClient.get("api/test")) as { message: string };
   }
 }
