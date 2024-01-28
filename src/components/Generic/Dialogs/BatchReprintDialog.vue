c2b84c1e8f2a72996b4347588367a329bae3dddd
David Zwart <davidzwa@gmail.com>
2024-01-28 22:23:48 +0100

    feat: batch reprint dialog, compose grid view

---
+++ src/components/Generic/Dialogs/BatchReprintDialog.vue
@@ -0,0 +1,25 @@
+<template>
+  <BaseDialog :id="dialog.dialogId" max-width="700px" @escape="closeDialog()">
+    Dialog shown
+  </BaseDialog>
+</template>
+<script setup>
+import { DialogName } from "@/components/Generic/Dialogs/dialog.constants";
+import { useDialog } from "@/shared/dialog.composable";
+import { onMounted, watch } from "vue";
+import { usePrinterStore } from "@/store/printer.store";
+
+const dialog = useDialog(DialogName.BatchReprintDialog);
+const printerStore = usePrinterStore();
+
+function closeDialog() {
+  dialog.closeDialog();
+}
+
+watch(
+  () => printerStore.selectedPrinters.length,
+  () => {
+    console.log("Selection changed");
+  }
+);
+</script>
