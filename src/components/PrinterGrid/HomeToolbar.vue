1a265458da9efb23d648599bc159f2eaaf6852cf
David Zwart <davidzwa@gmail.com>
2024-11-14 13:14:18 +0100

    feat: redesign printer grid layout for more flexibility (odd columns, etc)

--- src/components/PrinterGrid/HomeToolbar.vue
+++ src/components/PrinterGrid/HomeToolbar.vue
@@ -8,7 +8,7 @@
       rounded
       @change="changeFloorIndex"
     >
-      <v-btn v-for="f in floors" :key="f._id">
+      <v-btn v-for="f in floors" :key="f.id">
         <v-icon>format_align_left</v-icon>
         {{ f.name }}
       </v-btn>
@@ -23,6 +23,30 @@
     </v-alert>
     <div class="ma-4 pt-6">
       <v-switch v-model="gridStore.gridEditMode" label="Printer Relocate Mode"></v-switch>
+    </div>
+
+    <!-- Optional grid controls -->
+    <div class="controls-container ma-4">
+      <div class="d-flex gap-4">
+        <v-text-field
+          :value="rows"
+          @change="updateGridRows"
+          type="number"
+          label="Rows"
+          :min="1"
+          density="compact"
+          hide-details
+        />
+        <v-text-field
+          :value="columns"
+          @change="updateGridColumns"
+          type="number"
+          label="Columns"
+          :min="1"
+          density="compact"
+          hide-details
+        />
+      </div>
     </div>
 
     <v-spacer></v-spacer>
@@ -57,19 +81,54 @@ import { usePrinterStore } from "@/store/printer.store";
 import { useGridStore } from "@/store/grid.store";
 import { useFloorStore } from "@/store/floor.store";
 import { usePrinterStateStore } from "@/store/printer-state.store";
+import { useSettingsStore } from "@/store/settings.store";
 
 const printerStore = usePrinterStore();
 const printerStateStore = usePrinterStateStore();
 const floorStore = useFloorStore();
 const gridStore = useGridStore();
+const settingsStore = useSettingsStore();
+
+const loading = ref<boolean>(false);
 const selectedFloorToggleIndex = ref<number>(0);
 
 const floors = computed(() => {
   return floorStore.floors;
 });
+const columns = computed(() => settingsStore.gridCols);
+const rows = computed(() => settingsStore.gridRows);
 
 function changeFloorIndex(index: any) {
   floorStore.changeSelectedFloorByIndex(index);
   selectedFloorToggleIndex.value = index;
 }
+
+async function updateGridRows(newRows: number) {
+  return updateGridSettings(newRows, columns.value);
+}
+
+async function updateGridColumns(newColumns: number) {
+  return updateGridSettings(rows.value, newColumns);
+}
+
+async function updateGridSettings(rows: number, columns: number) {
+  try {
+    loading.value = true;
+    await settingsStore.updateFrontendSettings({
+      gridRows: rows,
+      gridCols: columns,
+      largeTiles: settingsStore.largeTiles,
+    });
+  } catch (e) {
+    console.log(e);
+  } finally {
+    loading.value = false;
+  }
+}
 </script>
+
+<style scoped>
+.controls-container {
+  max-width: 400px;
+}
+</style>
--- src/shared/printer-grid.constants.ts
+++ src/shared/printer-grid.constants.ts
@@ -1,5 +1,2 @@
-// Should not be adjusted unless the grid system is changed to support other than 12
-export const totalVuetifyColumnCount = 12;
-
-export const rowOptions = [1, 2, 3, 4, 5, 6, 7, 8].map((v) => v * 2);
-export const colOptions = [1, 2, 3, 4, 6].map((v) => v * 2);
+export const rowOptions = [1, 2, 3, 4, 5, 6, 7, 8];
+export const colOptions = [1, 2, 3, 4, 5, 6, 7, 8];
