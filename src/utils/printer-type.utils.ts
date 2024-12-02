145f3e401645321c6c4fbafd8fbfd1fd907e7635
David Zwart <davidzwa@gmail.com>
2024-12-02 22:25:21 +0100

    feat: add better bambu validation and name detection

--- src/utils/printer-type.utils.ts
+++ src/utils/printer-type.utils.ts
@@ -6,10 +6,16 @@ export function isMoonrakerType(printerType?: number) {
   return printerType === 1;
 }
 
+export function isBambuType(printerType?: number) {
+  return printerType === 2;
+}
+
 export function getServiceName(printerType?: number) {
   return isOctoPrintType(printerType)
     ? "OctoPrint"
     : isMoonrakerType(printerType)
     ? "Moonraker"
+    : isBambuType(printerType)
+    ? "Bambu"
     : "Unknown";
 }
