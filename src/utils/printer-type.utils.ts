406a81f99ac79efba38144a49c80a32d918ebdd0
David Zwart <davidzwa@gmail.com>
2025-03-23 07:29:56 +0100

    feat: add prusa link to printer create dialog

--- src/utils/printer-type.utils.ts
+++ src/utils/printer-type.utils.ts
@@ -1,9 +1,17 @@
+export const OctoPrintType = 0;
+export const MoonrakerType = 1;
+export const PrusaLinkType = 2;
+
 export function isOctoPrintType(printerType?: number) {
-  return printerType === 0;
+  return printerType === OctoPrintType;
 }
 
 export function isMoonrakerType(printerType?: number) {
-  return printerType === 1;
+  return printerType === MoonrakerType;
+}
+
+export function isPrusaLinkType(printerType?: number) {
+  return printerType === PrusaLinkType;
 }
 
 export function getServiceName(printerType?: number) {
@@ -11,5 +19,7 @@ export function getServiceName(printerType?: number) {
     ? "OctoPrint"
     : isMoonrakerType(printerType)
     ? "Moonraker"
+    : isPrusaLinkType(printerType)
+    ? "PrusaLink"
     : "Unknown";
 }
